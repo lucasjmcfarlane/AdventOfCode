f1c8b09447e4df865cf9ae24513466ed2c559c3f
Lucas McFarlane <126113690+lucasjmcfarlane@users.noreply.github.com>
2023-08-25 00:56:23 -0500

    Complete Day 7

--- Java/2015/Day7/Day7.java
+++ Java/2015/Day7/Day7.java
@@ -3,14 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//create an object for every instruction in the txt
-//object will have a string array length 2 called requires
-//instructions may require 0 1 or 2 wires to already exist
-//first loop will complete all instructions that require 0 existing wires
-//second loop will complete instructions that require those wires and nothing more
-//this repeats until the queue is empty
-
-public class Day7Part1 {
+public class Day7{
     public static void main(String[] args) {
         try{
             File file = new File("Day7Input.txt");
@@ -18,29 +11,26 @@ public class Day7Part1 {
             ArrayList<Instruction> instructions = new ArrayList<Instruction>();
             ArrayList<Wire> wires = new ArrayList<Wire>();
 
-            //fill instructions arrayList
             while(sc.hasNextLine()){
                 String input = sc.nextLine();
                 String[] inputArr = input.split(" ");
-                Instruction newInstruction;
 
                 if(input.contains("AND") || input.contains("OR") || input.contains("LSHIFT") || input.contains("RSHIFT")){
-                    newInstruction = new Instruction(inputArr[4], inputArr[1], inputArr[0], inputArr[2]);
+                    instructions.add(new Instruction(inputArr[4], inputArr[1], inputArr[0], inputArr[2]));
                 }
                 else if(input.contains("NOT")){
-                    newInstruction = new Instruction(inputArr[3], inputArr[0], inputArr[1], null);
+                    instructions.add(new Instruction(inputArr[3], inputArr[0], inputArr[1], null));
                 }
                 else{
-                    newInstruction = new Instruction(inputArr[2], inputArr[1], inputArr[0], null);
+                    instructions.add(new Instruction(inputArr[2], inputArr[1], inputArr[0], null));
                 }
-                instructions.add(newInstruction);
             }
 
             //loop through instructions until list is empty
             while(!instructions.isEmpty()){
                 Instruction instruction = instructions.remove(0);
-                int val1 = getValFromIdent(instruction.operands[0], wires);
-                int val2 = getValFromIdent(instruction.operands[1], wires);
+                int val1 = operandToInt(instruction.operands[0], wires);
+                int val2 = operandToInt(instruction.operands[1], wires);
 
                 if((val1 > -1 && val2 == -2) || (val1 > -1 && val2 > -1)){
                     wires.add(new Wire(instruction.identifier, performOperation(val1, val2, instruction.operator)));
@@ -51,7 +41,7 @@ public class Day7Part1 {
             }
 
             for(int i = 0; i<wires.size(); i++){
-                System.out.println(wires.get(i).ident+": "+wires.get(i).value);
+                if(wires.get(i).ident.equals("a")) System.out.println("a="+wires.get(i).value);
             }
             sc.close();
         }
@@ -61,7 +51,7 @@ public class Day7Part1 {
         }
     }
 
-    public static int getValFromIdent(String ident, ArrayList<Wire> wires){
+    public static int operandToInt(String ident, ArrayList<Wire> wires){
         if(ident==null) return -2;
         try{
             return Integer.parseInt(ident);
@@ -75,9 +65,16 @@ public class Day7Part1 {
     }
 
     public static int performOperation(int val1, int val2, String operator){
-        return 1;
+        switch(operator){
+            case "->":     return val1;
+            case "AND":    return val1&val2;
+            case "OR":     return val1|val2;
+            case "NOT":    return 65536 + ~val1;
+            case "LSHIFT": return val1<<val2;
+            case "RSHIFT": return val1>>val2;
+        }
+        return -1;
     }
-
 }
 
 class Wire{
